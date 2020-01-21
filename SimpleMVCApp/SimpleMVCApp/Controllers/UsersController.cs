c25d2e20e68a3db3dab56089110328e43ddede57
ascutariu <andreiscutariu92@gmail.com>
2020-01-21 20:17:04 +0200

    updates

--- SimpleMVCApp/SimpleMVCApp/Controllers/UsersController.cs
+++ SimpleMVCApp/SimpleMVCApp/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿namespace SimpleMVCApp.Controllers
 {
     using System.Collections.Generic;
+    using System.Collections.Specialized;
     using System.Web.Mvc;
     using Models;
 
@@ -14,6 +15,7 @@
         }
 
         //users/index
+        [HttpGet]
         public ActionResult Index()
         {
             List<User> users = this.userData.GetUsers();
@@ -21,11 +23,40 @@
             return this.View(users);
         }
 
+        [HttpGet]
         public ActionResult ById(int id)
         {
             var u = this.userData.GetUser(id);
 
             return this.View(u);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser()
+        {
+            NameValueCollection form = this.Request.Form;
+
+            var userName = form["Name"];
+
+            var email = form["Email"];
+
+            return this.View("Create");
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser2(User u)
+        {
+            this.userData.AddUser(u);
+
+            return RedirectToAction("Index");
+
+            //return this.View("Index");
         }
     }
 }
---
+++ SimpleMVCApp/SimpleMVCApp/Models/UserData.cs
@@ -0,0 +1,39 @@
+﻿namespace SimpleMVCApp.Models
+{
+    using System.Collections.Generic;
+
+    public class UserData
+    {
+        private static readonly List<User> Users = new List<User>();
+
+        private int currentIdx = 2;
+
+        static UserData()
+        {
+            Users.Add(new User { Id = 1, Email = "and@yahoo.com", Name = "and" });
+            Users.Add(new User { Id = 2, Email = "mih@yahoo.com", Name = "mih" });
+        }
+
+        public UserData()
+        {
+        }
+
+        public List<User> GetUsers()
+        {
+            return Users;
+        }
+
+        public User GetUser(int id)
+        {
+            return Users.Find(x => x.Id == id);
+        }
+
+        public void AddUser(User u)
+        {
+            u.Id = currentIdx + 1;
+            currentIdx++;
+
+            Users.Add(u);
+        }
+    }
+}
